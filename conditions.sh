84d800467a00f2b02d7927b611b317876238b173
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-23 11:05:55 +0530

    message

--- conditions.sh
+++ conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=10
 
-if [ $NUMBER -lt 10]; then
+if [ $NUMBER -lt 10 ]; then
     echo " Given number $NUMBER is lessthn 10"
 else
     echo " Given number $NUMBER is greater than or equal to 10"
