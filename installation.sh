70eeb092139e3852c140730e2fe16a292e321956
Akhil Naidu <akhilnaid1997@gmail.com>
2025-09-23 12:19:12 +0530

    message

--- installation.sh
+++ installation.sh
@@ -3,11 +3,12 @@
 USERID=$(id -u) # getting the user id and copying to variable.
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: You donot have sudo permissions, please install with sudo permissions"\
-    exit 1
+    echo "ERROR:: You donot have sudo permissions, please install with sudo permissions"
+    exit 1 
 fi
 
 dnf install mysql -y
+
 if [ $? -eq 0 ]; then
     echo "Installation of mysql is successful"
 else
